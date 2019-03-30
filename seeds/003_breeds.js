8d91db35845d5ddda06541eadc5eaa8d34a5c983
Mojjammil Khandker <mojjammil@localhost.localdomain>
2019-03-31 00:02:55 +0200

    Added fake data

---
+++ seeds/003_breeds.js
@@ -0,0 +1,15 @@
+const faker = require('faker');
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('breeds').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('breeds').insert([
+        {"name": faker.commerce.color()},
+        {"name": faker.commerce.color()},
+        {"name": faker.commerce.color()},
+        {"name": faker.commerce.color()},
+        {"name": faker.commerce.color()}
+      ]);
+    });
+};
---
+++ seeds/005_admins.js
@@ -0,0 +1,25 @@
+const faker = require('faker');
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('admins').truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('admins').insert([
+       {
+       "kennel_id": 1,
+       "username": faker.internet.userName(),
+       "password": faker.internet.password(),
+       },
+       {
+       "kennel_id": 2,
+       "username": faker.internet.userName(),
+       "password": faker.internet.password(),
+       },
+       {
+       "kennel_id": 3,
+       "username": faker.internet.userName(),
+       "password": faker.internet.password(),
+       }
+      ]);
+    });
+};
